93dc109210c6ceddb7201522f3806c278b7ba71b
Khánh Khánh <66833938+CromBorw2e36@users.noreply.github.com>
2026-02-08 11:31:13 +0700

    update version 2.2.2

---
+++ dist/drawpdf.full.js
@@ -0,0 +1,14 @@
+import { C as s, D as r, F, b as P, J as T, P as i, a as t, R as D, T as d, V as n, D as o } from "./index.full-C5Yymw4C.js";
+export {
+  s as CKEditorParser,
+  r as DrawPDF,
+  F as FONTS,
+  P as FONT_CONFIG,
+  T as JsPdfService,
+  i as PAGE,
+  t as PDFRenderer,
+  D as RichTextTokenizer,
+  d as TemplateEngine,
+  n as VERSION,
+  o as default
+};
